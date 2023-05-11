46a877951156c372081ff7a0c57f92d94e9b09a4
Evgenii <uvaz2014@mail.ru>
2023-05-11 21:13:50 +0300

    main restored

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Pre-processing
+echo "Running pre-processing"
+bash scripts/preprocess.sh
+
 # Run the big data pipeline
 echo "Running Stage 1 of the pipeline - PostgreSQL + Sqoop"
 bash scripts/stage1.sh
@@ -12,6 +16,10 @@ bash scripts/stage3.sh
 
 echo "Running Stage 4 of the pipeline - Streamlit"
 bash scripts/stage4.sh
+
+# Post-processing 
+echo "Running post-processing!"
+bash scripts/postprocess.sh
 
 
 # Check the quality of the codes
